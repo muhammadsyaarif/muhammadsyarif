36dfd9146d86b75afa731881d9a7b6103b0ce658
muhammadsyaarif <148513751+muhammadsyaarif@users.noreply.github.com>
2024-07-22 09:25:56 +0700

    Update page.tsx

--- app/all-projects/page.tsx
+++ app/all-projects/page.tsx
@@ -64,7 +64,7 @@ function page() {
               <Image
                 className="h-full w-full rounded-lg object-cover"
                 src={
-                  "https://res.cloudinary.com/dgnmqbglc/image/upload/v1717451950/Syarif/notes_aztd52.png"
+                  "https://res.cloudinary.com/dvavtg6tx/image/upload/v1721614872/Screenshot_355_lxuvex.png"
                 }
                 alt={"coming-soon"}
                 width={300}
