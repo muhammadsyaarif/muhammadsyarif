10eadc7be55b0afdcc7205ed8d64d6c47184443e
muhammadsyaarif <148513751+muhammadsyaarif@users.noreply.github.com>
2024-07-22 09:12:57 +0700

    Update Works.tsx

--- components/Works.tsx
+++ components/Works.tsx
@@ -27,7 +27,7 @@ function Insight() {
                 <Image
                   className="h-full w-full rounded-lg object-cover"
                   src={
-                    "https://res.cloudinary.com/dgnmqbglc/image/upload/v1717409364/Syarif/smartdam_gpobgy.png"
+                    "https://res.cloudinary.com/dvavtg6tx/image/upload/v1721573924/Screenshot_279_hvuday.png"
                   }
                   alt={"coming-soon"}
                   width={300}
@@ -101,7 +101,7 @@ function Insight() {
                 <Image
                   className="h-full w-full rounded-lg object-cover"
                   src={
-                    "https://res.cloudinary.com/dgnmqbglc/image/upload/v1717425211/Syarif/vaksin2_k2rve8.png"
+                    "https://res.cloudinary.com/dvavtg6tx/image/upload/v1721613761/Screenshot_352_rppbpy.png"
                   }
                   alt={"coming-soon"}
                   width={300}
@@ -138,7 +138,7 @@ function Insight() {
                 <Image
                   className="h-full w-full rounded-lg object-cover"
                   src={
-                    "https://res.cloudinary.com/dgnmqbglc/image/upload/v1717410478/Syarif/catering3_hl7iit.png"
+                    "https://res.cloudinary.com/dvavtg6tx/image/upload/v1721573912/Screenshot_350_hqmbdi.png"
                   }
                   alt={"coming-soon"}
                   width={300}
